f3d1e4008010906312955c18e7e6291406991a9c
Mulisa <mulisa0998@gmail.com>
2021-03-20 12:22:36 +0200

    moved input editText manipulation to TableViewController and implemented functions to display results

--- app/src/main/java/com/puzzle/industries/inumbr/ui/MainActivity.java
+++ app/src/main/java/com/puzzle/industries/inumbr/ui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.puzzle.industries.inumbr.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
@@ -16,12 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.puzzle.industries.inumbr.R;
+import com.puzzle.industries.inumbr.adapters.TableViewController;
 import com.puzzle.industries.inumbr.databinding.ActivityMainBinding;
+import com.puzzle.industries.inumbr.databinding.ItemCombinationsBinding;
 import com.puzzle.industries.inumbr.viewModels.MainActivityViewModel;
 
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
     private MainActivityViewModel mViewModel;
-    private EditText[] mNumInputList;
+    private TableViewController tableViewController;
+    private Resources res;
+
+    private double ballWidth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +44,23 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
+        res = getResources();
+        ballWidth = res.getDimension(R.dimen.col_width);
+
         mViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-
-        mNumInputList = new EditText[]{
-                mBinding.edtCombIn1,
-                mBinding.edtCombIn2,
-                mBinding.edtCombIn3,
-                mBinding.edtCombIn4,
-                mBinding.edtCombIn5,
-                mBinding.edtCombIn6
-        };
-
-        setInputFlow();
-
         mViewModel.getCombinations().observe(this, this::displayTheCombinations);
 
+        tableViewController = new TableViewController(mBinding.tlNumberInputs, ballWidth);
+
+        mBinding.ibtnAddNumber.setOnClickListener(v -> tableViewController.addNumberInput());
+
+        mBinding.btnClear.setOnClickListener(v -> {
+            mBinding.llCombHolder.removeAllViews();
+            tableViewController.removeAllViews();
+        });
+
         mBinding.btnCombGen.setOnClickListener(v -> {
-            List<Integer> selectedNumbers = getSelectedNumbers();
+            List<Integer> selectedNumbers = tableViewController.getSelectedNumbers();
             Collections.sort(selectedNumbers);
             String sMaxLengthForComb = mBinding.edtCombLength.getText().toString();
 
@@ -76,14 +82,13 @@ public class MainActivity extends AppCompatActivity {
                     for (int i = 1; i < selectedNumbers.size(); i++){
                         if (selectedNumbers.get(i).equals(selectedNumbers.get(i - 1))){
                             containsDuplicates = true;
+                            Toast.makeText(this, "Error, there's a duplicate of " + selectedNumbers.get(i),
+                                    Toast.LENGTH_SHORT).show();
                             break;
                         }
                     }
 
-                    if (containsDuplicates){
-                        Toast.makeText(this, "Error, selected numbers contain duplicates", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
+                    if (!containsDuplicates){
                         generateCombinations(selectedNumbers, maxLength);
                     }
                 }
@@ -91,53 +96,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void displayTheCombinations(List<String> listOfCombinations) {
+    private void displayTheCombinations(List<int[]> listOfCombinations) {
         mBinding.llCombHolder.removeAllViews();
+        int margin = (int) res.getDimension(R.dimen.mp_8dp);
 
-        for (String comb : listOfCombinations){
-            TextView txtComb = new TextView(this);
-            txtComb.setText(comb);
-            txtComb.setTextSize(getResources().getDimension(R.dimen.txt_medium));
-            txtComb.setGravity(Gravity.CENTER);
+        for (int[] comb : listOfCombinations){
+
+            ItemCombinationsBinding combBinding = ItemCombinationsBinding.inflate(getLayoutInflater());
+            TableViewController controller = new TableViewController(combBinding.tlResultItem, ballWidth);
+
+            for (int i : comb) controller.addNumber(i);
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT);
-            params.setMargins(8, 8, 8, 8);
+            params.setMargins(0, margin, 0, margin);
 
-            mBinding.llCombHolder.addView(txtComb, params);
+            mBinding.llCombHolder.addView(combBinding.getRoot(), params);
         }
     }
 
     private void generateCombinations(List<Integer> selectedNumbers, int maxLength) {
         mViewModel.generateCombinations(selectedNumbers, maxLength);
     }
-
-    private List<Integer> getSelectedNumbers() {
-        List<Integer> selectedNumbers = new ArrayList<>();
-
-        for (EditText edtCombIn : mNumInputList){
-           String in = edtCombIn.getText().toString();
-           if (!in.isEmpty()){
-               selectedNumbers.add(Integer.parseInt(in));
-           }
-        }
-
-        return selectedNumbers;
-    }
-
-    private void setInputFlow(){
-        for (int i = 0; i < 5; i++){
-            EditText edtComb = mNumInputList[i];
-
-            int finalI = i;
-            edtComb.setOnEditorActionListener((v, actionId, event) -> {
-                if (actionId == EditorInfo.IME_ACTION_NEXT){
-                    mNumInputList[finalI + 1].requestFocus();
-                }
-                return false;
-            });
-        }
-    }
-
 
 }
