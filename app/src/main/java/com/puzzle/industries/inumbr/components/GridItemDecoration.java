724224b964c9489b6d5abe3f9120486770cff6e8
Mulisa <mulisa0998@gmail.com>
2021-04-15 09:27:18 +0200

    implemented GridLayout decorator

---
+++ app/src/main/java/com/puzzle/industries/inumbr/components/GridItemDecoration.java
@@ -0,0 +1,24 @@
+package com.puzzle.industries.inumbr.components;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GridItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int margin;
+
+    public GridItemDecoration(int margin) {
+        this.margin = margin;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
+                               @NonNull RecyclerView parent,
+                               @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.set(margin, margin, margin, margin);
+    }
+}
