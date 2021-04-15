d43d135cf8123d22176a606f555a3ea92227c5cd
Mulisa <mulisa0998@gmail.com>
2021-04-15 09:36:01 +0200

    implemented Credentials repository

---
+++ app/src/main/java/com/puzzle/industries/inumbr/repositores/CredentialsRepository.java
@@ -0,0 +1,26 @@
+package com.puzzle.industries.inumbr.repositores;
+
+import com.puzzle.industries.inumbr.dataModels.PreferenceModel;
+
+public class CredentialsRepository {
+
+    private final PreferenceModel PREF_MODEL = new PreferenceModel();
+    private static final CredentialsRepository instance = new CredentialsRepository();
+
+    public static CredentialsRepository getInstance(){
+        return instance;
+    }
+
+    public void setCredentials(String phone, String password){
+        PREF_MODEL.setCredentials(phone, password);
+    }
+
+    public String getPhone(){
+        return PREF_MODEL.getMobileNumber();
+    }
+
+    public String getPassword(){
+        return PREF_MODEL.getPassword();
+    }
+
+}
