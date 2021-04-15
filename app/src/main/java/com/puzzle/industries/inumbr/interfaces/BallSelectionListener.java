23056e5734a81a78ed75c2089007cfa238e9427f
Mulisa <mulisa0998@gmail.com>
2021-04-15 09:32:26 +0200

    created BallSelectionListener interface

---
+++ app/src/main/java/com/puzzle/industries/inumbr/interfaces/BallSelectionListener.java
@@ -0,0 +1,8 @@
+package com.puzzle.industries.inumbr.interfaces;
+
+import com.puzzle.industries.inumbr.dataModels.Ball;
+
+public interface BallSelectionListener {
+
+    void ballSelected(Ball ball);
+}
