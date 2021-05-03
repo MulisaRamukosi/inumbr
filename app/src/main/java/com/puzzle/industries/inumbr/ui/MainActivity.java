213d793755b497ff73201fc4a2a18e207a4e5e5d
Mulisa <mulisa0998@gmail.com>
2021-05-03 14:54:24 +0200

    moved ball selection and combo generation implementation to GameViewActivity

--- app/src/main/java/com/puzzle/industries/inumbr/ui/MainActivity.java
+++ app/src/main/java/com/puzzle/industries/inumbr/ui/MainActivity.java
@@ -20,30 +20,16 @@ public class MainActivity extends FragmentActivity  {
 
     private FragmentManager mFragmentManager;
     private ActivityMainBinding mBinding;
-    private InputBottomSheet mInputBottomSheet;
 
-    private final List<Ball> SELECTED_BALLS = new ArrayList<>();
-    private final List<Ball> SELECTION_BALLS = new ArrayList<>();
-
-    private final BallsAdapter SELECTED_BALLS_ADAPTER = new BallsAdapter(SELECTED_BALLS, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
 
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         mFragmentManager = getSupportFragmentManager();
 
-        //generate selection balls
-        for (int i = 1; i <= 49; i++){
-            SELECTION_BALLS.add(new Ball(i));
-        }
-
-        int margins = (int) getResources().getDimension(R.dimen.mp_4dp);
-
-        mBinding.rvSelectedNums.addItemDecoration(new GridItemDecoration(margins));
-        mBinding.rvSelectedNums.setAdapter(SELECTED_BALLS_ADAPTER);
+        setContentView(mBinding.getRoot());
 
         mBinding.btnSetCredentials.setOnClickListener(v -> {
             CredentialsBottomSheet credentialsBottomSheet = new CredentialsBottomSheet();
@@ -59,34 +45,5 @@ public class MainActivity extends FragmentActivity  {
         LuckyNumberAdapter luckyNumberAdapter = new LuckyNumberAdapter(luckyNumberGames);
         mBinding.rvLuckyNumberGames.setAdapter(luckyNumberAdapter);
         mBinding.rvLuckyNumberGames.addItemDecoration(new GridItemDecoration(margins));
-    }
-
-    private void displayResults(List<int[]> listOfCombinations) {
-        ResultsActivity.sResults = listOfCombinations;
-        startActivity(new Intent(MainActivity.this, ResultsActivity.class));
-    }
-
-    private void generateCombinations(int maxLength) {
-        List<Integer> selectedBalls = new ArrayList<>();
-        for (Ball b : SELECTED_BALLS){
-            selectedBalls.add(b.getNum());
-        }
-        new GenerateComboTask(selectedBalls, selectedBalls.size(), maxLength, this::displayResults).execute();
-    }
-
-    @Override
-    public void ballSelected(Ball ball) {
-        int ballPosition = SELECTED_BALLS.indexOf(ball);
-
-        if (ballPosition == -1){
-            SELECTED_BALLS.add(ball);
-            SELECTED_BALLS_ADAPTER.notifyItemInserted(SELECTED_BALLS.size() - 1);
-        }
-        else{
-            SELECTED_BALLS.remove(ballPosition);
-            SELECTED_BALLS_ADAPTER.notifyItemRemoved(ballPosition);
-        }
-
-        mBinding.btnClearSelection.setVisibility(SELECTED_BALLS.isEmpty() ? View.GONE : View.VISIBLE);
     }
 }
